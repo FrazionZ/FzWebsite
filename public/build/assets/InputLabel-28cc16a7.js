d2adbc2279a7e83b462773089086f282e940898e
Florent Calabrési <florent.cala@gmail.com>
2023-02-19 18:54:53 +0100

    Fix crash profile page

---
+++ public/build/assets/InputLabel-28cc16a7.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app-e32e67e4.js";function n({forInput:a,value:r,className:l,children:o}){return t("label",{htmlFor:a,className:"block "+l,children:r||o})}export{n as I};
--- public/build/assets/InputLabel-45ab080e.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./app-af5531a3.js";function n({forInput:a,value:r,className:l,children:o}){return t("label",{htmlFor:a,className:"block "+l,children:r||o})}export{n as I};
