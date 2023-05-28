c9405e94f7c2a3477dba6e3f9d7c208df70f537d
Florent Calabrési <florent.cala@gmail.com>
2023-05-29 00:02:51 +0200

    Remove badge in notifications if list is empty

--- resources/js/Components/DropdownNotifications.jsx
+++ resources/js/Components/DropdownNotifications.jsx
@@ -20,7 +20,7 @@ export default function DropdownNotifications() {
                 <Menu.Button style={{ background: 'transparent' }} className="inline-flex w-full justify-center gap-x-1.5 rounded-md p-3 text-sm font-semibold shadow-sm">
                     <FaBell style={{ fill: "#fff", width: "24px", height: "24px" }} />
                 </Menu.Button>
-                <span className='badge'>{notifications.length}</span>
+                <span className={`badge ${notifications.length < 1 ? "hidden" : ""}`}>{notifications.length}</span>
             </div>
 
             <Transition
@@ -32,24 +32,34 @@ export default function DropdownNotifications() {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                        {notifications.map((notification, index) => {
-                            return (
-                                <Menu.Item key={index}>
-                                    {({ active }) => (
-                                        <a
-                                            className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                            )}
-                                        >
-                                            {notification.enum}
-                                        </a>
-                                    )}
-                                </Menu.Item>
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[var(--fzbg-2)] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="p-2">
+                        {notifications.length == 0 && 
+                            ( <span>Aucune notifications</span> )
+                        }
+                        {notifications.length > 0 &&
+                            (
+                                <>
+                                    {notifications.map((notification, index) => {
+                                        return (
+                                            <Menu.Item key={index}>
+                                                {({ active }) => (
+                                                    <a
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        {notification.enum}
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        )
+                                    })}
+                                </>
                             )
-                        })}
+                        }
+
 
                     </div>
                 </Menu.Items>
