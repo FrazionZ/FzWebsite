7f9301306545a80bcbc276858c9ddc1b5b96cf1e
Florent Calabrési <florent.cala@gmail.com>
2023-06-03 20:57:07 +0200

    Add axeptio

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,6 @@
         />
         <!-- Scripts -->
         <script>
-            alert('cc')
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
