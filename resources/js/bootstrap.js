092e041b1843114235bb0159cdecdbee9b7b5671
Florent Calabrési <florent.cala@gmail.com>
2023-06-03 21:09:15 +0200

    Add axeptio

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,7 +25,7 @@ window.axeptioSettings = {
 (function(d, s) {
       var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
       e.async = true; 
-      e.src = "//static.axept.io/sdk-slim.js";
+      e.src = "axeptio.js";
       t.parentNode.insertBefore(e, t);
 })(document, "script");
 
