8d75b2ba77d6580cc00bfbb957906b4d9aa10a0f
Florent Calabrési <florent.cala@gmail.com>
2023-06-05 16:23:44 +0200

    Added axeptio for cookies consents

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,6 +18,16 @@ Array.prototype.swap = function (x, y) {
     return this;
 };
 
+window.axeptioSettings = {
+    clientId: "647b861ef54636749ca8c5f4",
+};
+
+(function (d, s) {
+    var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+    e.async = true; e.src = "//static.axept.io/sdk.js";
+    t.parentNode.insertBefore(e, t);
+})(document, "script");
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
