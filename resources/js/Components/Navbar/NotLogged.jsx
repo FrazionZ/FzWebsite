27fdf895955132fc97597bc103a1aa00c331457f
= <florent.cala@gmail.com>
2024-06-29 23:35:30 +0200

    Remove node html image package

--- resources/js/Components/Navbar/NotLogged.jsx
+++ resources/js/Components/Navbar/NotLogged.jsx
@@ -1,13 +1,12 @@
 import { Link } from "@inertiajs/react";
 import { BiLogInCircle, BiUserPlus } from 'react-icons/bi'
-
+import { BsMicrosoft } from 'react-icons/bs'
 export default function NotLogged(){
 
     return (
         <>
             <div className="not_logged">
-                <Link className="nav-link" href="/login">Connexion</Link>
-                <Link className="nav-link" href="/register">Inscription</Link>
+                <Link className="nav-link" style={{ gap: '10px' }} href="/login"><BsMicrosoft /> Se connecter avec Microsoft</Link>
             </div>
             <div className="not_logged mobile">
                 <Link className="nav-link" href="/login"><BiLogInCircle /></Link>
--- routes/auth.php
+++ routes/auth.php
@@ -13,31 +13,25 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+    //Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    //Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+    //Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.handle');
+    //Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.handle');
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
+    //Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');
+    //Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name('password.reset');
+    //Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
 
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.store');
+    //Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 });
 
 Route::middleware('fzauth')->group(function () {
-    Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
+    /*Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
@@ -55,8 +49,7 @@ Route::middleware('fzauth')->group(function () {
 
     Route::put('email', [EmailController::class, 'update'])->name('email.update');
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');*/
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
